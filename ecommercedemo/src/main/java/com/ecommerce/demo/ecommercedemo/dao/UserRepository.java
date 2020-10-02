ce61b7f909cd6b3cce0e8d04dddd654b35c05a96
Tuhin Debnath <Tuhin Debnath@DESKTOP-PRDLNOE>
2020-10-02 20:04:28 +0530

    commit1

--- ecommercedemo/src/main/java/com/ecommerce/demo/ecommercedemo/dao/UserRepository.java
+++ ecommercedemo/src/main/java/com/ecommerce/demo/ecommercedemo/dao/UserRepository.java
@@ -4,10 +4,8 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 import com.ecommerce.demo.ecommercedemo.entity.LoginData;
 
-
 @Repository
 public interface UserRepository extends JpaRepository<LoginData, String> {
-
 
 	public LoginData findByUserIdAndPassword(String email, String password);
 }
